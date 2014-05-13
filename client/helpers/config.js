af608afd95564e4c93fed387771decc22560a6de
Liucw <liucw.mail@gmail.com>
2014-05-13 23:56:57 +0800

    Update meteor version to 0.8.1.2

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,17 +1,18 @@
 moment.lang('en', {
     relativeTime : {
-        future: "在 %s",
-        past:   "%s 之前",
-        s:  "秒",
+        future: "在%s",
+        past:   "%s之前",
+        s:  "1秒钟",
+        ss: "%d秒",
         m:  "1分钟",
-        mm: "%d 分钟",
+        mm: "%d分钟",
         h:  "1小时",
-        hh: "%d 小时",
+        hh: "%d小时",
         d:  "1天",
-        dd: "%d 天",
+        dd: "%d天",
         M:  "1个月",
-        MM: "%d 个月",
+        MM: "%d个月",
         y:  "1年",
-        yy: "%d 年"
+        yy: "%d年"
     }
 });
