183326a45cd55b718643d2e332b5c10617ded6f4
bruce.w <carrot.live@gmail.com>
2014-10-23 17:18:41 +0800

    Add changePassword of user

---
+++ server/suser.js
@@ -0,0 +1,11 @@
+Meteor.methods({
+  setPassword:function(password){
+        check(password,String);
+        if (this.userId){
+          Accounts.setPassword(this.userId,password);
+        }else{
+          return false;
+        }
+  },
+});
+
