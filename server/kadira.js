357a4a6f0e2e46142508eeb6f1961250edaec5f0
Liucw <liucw.mail@gmail.com>
2014-11-10 00:44:20 +0800

    Add website statistics

--- server/kadira.js
+++ server/kadira.js
@@ -1,1 +1,2 @@
-Kadira.connect('Tt8PpzXw5RAtmRsAi', 'f8c4608a-1b4f-47af-b4cf-e2b193e3527d');
+if(process.env.NODE_ENV == 'production')
+    Kadira.connect('Tt8PpzXw5RAtmRsAi', 'f8c4608a-1b4f-47af-b4cf-e2b193e3527d');
