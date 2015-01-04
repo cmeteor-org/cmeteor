29718e5de5895898e6927ee85257c6a8e7be8ae3
David <David@strucoder.local>
2015-01-04 22:25:14 +0800

    The emergency repair about re-edit

--- server/methods.js
+++ server/methods.js
@@ -79,16 +79,16 @@ Meteor.methods({
 Meteor.methods({
     'commentSubmit': function(content, postId) {
         var user = Meteor.user();
-        var post = Posts.findOne(postId);
-
-        if (!post) {
-            throwError(404, '评论的文章不存在！');
-            return false;
-        }
         if (!validStringLength(content, 1, 1000, throwError.bind(null, 403, '标题的长度应该在1-1000之间！')))
             return false;
         if (!user) {
             throwError(403, '必须登录！');
+            return false;
+        }
+
+        var post = Posts.findOne(postId);
+        if (!post) {
+            throwError(404, '评论的文章不存在！');
             return false;
         }
 
