7b11629051828bee71dfca2a4b832040b6e028f0
strucoder <zdwloveschina@163.com>
2015-05-27 17:11:50 +0800

    Big upgrade

--- client/views/pages/postEdit.js
+++ client/views/pages/postEdit.js
@@ -1,53 +1,40 @@
-var postEditor = null;
+var cm;
+
+
 Template.postEdit.rendered = function() {
-    postEditor = cEdor('ta-post-content');  //the cMeteorEditor for postEdit
-    var onePost = Posts.findOne(Router.current().params['id']);
-    $('#ta-post-title').val(onePost.title);
-    postEditor.editor.setValue(onePost.content);
+    var options = {
+        element: $('#editor')[0]
+    };
+    var editor = new Editor(options);
+    cm = editor.codemirror;
 };
-//
-//Template.postEdit.helpers({
-//    post: function(){
-//        return Posts.findOne(Router.current().params['id']);
-//    }
-//});
+
+Template.postEdit.helpers({
+    postObj: function() {
+        return Posts.findOne(Router.current().params['id']);
+    }
+});
 
 Template.postEdit.events({
     'click #btn-post-edit': function(e){
         e.preventDefault();
-        clearErrors();
         var title = $('#ta-post-title').val();
-        var content = postEditor.editor.getValue();
-        if(!validStringLength(title, 2, 28, throwError.bind(null, '标题的长度应该在2-28之间！'))) {
-            return false
-        }
-        if(!validStringLength(content, 10, 10000, throwError.bind(null, '正文的长度应该在10-10000之间！'))) {
-            return false
+        var content = cm.getValue();
+        // var content = postEditor.editor.getValue();
+        if (title.length < 2 || title.length > 28) {
+            return flushMsg('标题的长度应该在2-28之间！');
         }
 
-        var postId = Posts.findOne(Router.current().params['id'])._id;
+        if (content.length < 10 || content.length > 10000) {
+            return flushMsg('正文的长度应该在10-10000之间！');
+        }
+
+        var postId = Router.current().params['id'];
         Meteor.call('postEdit', title, content, postId, function(err){
-            if(err)
-                return root.throwError(err.reason);
+            if(err){
+                return flushMsg(err.reason);
+            }
             Router.go('post', {id: postId});
         });
-    },
-    'click #btn-post-preview': function(e){
-        e.preventDefault();
-        clearErrors();
-        var btn = $(e.target);
-        var $preview = $('#p-post-preview');
-        var $content = $('#ta-post-content');
-        if(btn.attr('value') === 'preview'){
-            $preview.show().html(postEditor.mk(postEditor.editor.getValue()));
-            $content.hide();
-            btn.attr('value', 'modify');
-            btn.text('修改');
-        }else if(btn.attr('value') === 'modify'){
-            $preview.hide();
-            $content.show();
-            btn.attr('value', 'preview');
-            btn.text('预览');
-        }
     }
 });
